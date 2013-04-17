976b378b38ee10af893083140b5d5fca29f753fd
xubuntu 12.10 <maciej@ubuntu.(none)>
2013-04-17 03:18:01 -0700

    Png generation

--- src/python/cscdgenerator.py
+++ src/python/cscdgenerator.py
@@ -12,17 +12,18 @@ pngName = "name"
 public = "public"
 protected = "protected"
 private = "private"
-package = "package"
+internal = "internal"
 
 class AccessModifier:
   def __init__(self):
-    self.access=package
+    self.access=internal
   def setPublic(self):
     self.access=public
   def setProtected(self):
     self.access=protected
   def setPrivate(self):
     self.access=private
+
     
 class Type:
   def __init__(self,name):
@@ -58,7 +59,7 @@ class Iface:
     self.extends = []
     self.methods = []
     self.attributes = []
-  def setExtend(self,iface):
+  def addExtend(self,iface):
     self.extends.append(iface)
   def addMethod(self,meth):
     self.methods.append(meth)
@@ -94,10 +95,251 @@ class Representation:
     self.interfaces.append(iface)
 
 def createSample(rep):
-  pass
+  #rep = Representation()
+  intrn = AccessModifier()
+  publ = AccessModifier()
+  publ.setPublic()
+  priv = AccessModifier()
+  priv.setPrivate()
+  prot = AccessModifier()
+  prot.setProtected()
+  void = Type("Void")
+  
+  breath = Iface("Breath")
+  lungs = Type("Lungs")
+  lungsA = Attr("lungs",lungs,priv)
+  breath.addAttribute(lungsA)
+  respire=Method("respire",void,priv)
+  breath.addMethod(respire)
+  rep.addInterface(breath)
+  eat = Iface("Eat")
+  mouth = Type("Mouth")
+  mouthA = Attr("mouth",mouth,prot)
+  energy = Type("Energy")
+  consume = Method("consume",energy,priv)
+  meal = Type("Meal")
+  food=Parameter(meal,"food")
+  consume.addParameter(food)
+  eat.addMethod(consume)
+  eat.addAttribute(mouthA)
+  rep.addInterface(eat)
+  
+  live = Iface("Live")
+  live.addExtend(breath)
+  live.addExtend(eat)
+  body = Type("Body")
+  bodyA = Attr("body",body,prot)
+  eat.addAttribute(bodyA)
+  exist=Method("exist",void,priv)
+  live.addMethod(exist)
+  rep.addInterface(live)
+  
+  bite = Iface("Bite")
+  teeth = Type("Teeth")
+  teethA = Attr("teeth",teeth,priv)
+  bite.addAttribute(teethA)
+  hurt = Type("Hurt")
+  attack=Method("attack",hurt,prot)
+  attack.isAbstract(True)
+  bite.addMethod(attack)
+  rep.addInterface(bite)
+  familiar = Iface("Familiar")
+  cares = Method("cares",void,intrn)
+  cares.isAbstract(True)
+  familiar.addMethod(cares)
+  rep.addInterface(familiar)
+  
+  animal = Cl("Animal")
+  bone = Type("Bone")
+  boneA = Attr("bone",bone,publ)
+  animal.addAttribute(boneA)
+  animal.addImplement(live)
+  move = Method("move",void,prot)
+  animal.addMethod(move)
+  animal.isAbstract(True)
+  rep.addClass(animal)
+  
+  
+  pet = Cl("Pet")
+  pet.setExtend(animal)
+  pet.addImplement(familiar)
+  pet.isAbstract(True)
+  friendliness = Type("Friendliness")
+  friendlinessA = Attr("friendliness",friendliness,publ)
+  pet.addMethod(cares)
+  pet.addAttribute(friendlinessA)
+  rep.addClass(pet)
+  
+  wild = Cl("Wild")
+  wild.setExtend(animal)
+  wild.addImplement(bite)
+  wild.isAbstract(True)
+  wildness = Type("Wildness")
+  wildnessA = Attr("wildness",wildness,publ)
+  wild.addAttribute(wildnessA)
+  wild.addMethod(attack)
+  rep.addClass(wild)
+  
+  dog = Cl("Dog")
+  dog.setExtend(pet)
+  caresImpl = Method("cares",void,intrn)
+  dog.addMethod(caresImpl)
+  huntCat = Method("huntCat", void, prot)
+  catT = Type("Cat")
+  catP=Parameter(catT,"cat")
+  huntCat.addParameter(catP)
+  energyT = Type("Energy")
+  energyP=Parameter(energyT,"energy")
+  huntCat.addParameter(energyP)
+  dog.addMethod(huntCat)
+  rep.addClass(dog)
+  
+  cat = Cl("Cat")
+  cat.setExtend(pet)
+  cat.addMethod(caresImpl)
+  mouw = Method("mouw",void,prot)
+  cat.addMethod(mouw)
+  rep.addClass(cat)
+  
+  cow = Cl("Cow")
+  cow.setExtend(pet)
+  milk = Type("Milk")
+  milkA = Attr("milk",milk,intrn)
+  cow.addAttribute(milkA)
+  cow.addMethod(caresImpl)
+  giveMilk = Method("giveMilk",milk,publ)
+  cow.addMethod(giveMilk)
+  rep.addClass(cow)
+  
+  tiger = Cl("Tiger")
+  tiger.setExtend(wild)
+  stripe = Type("Stripe")
+  stripeA = Attr("stripe",stripe,prot)
+  tiger.addAttribute(stripeA)
+  attackImpl = Method("attack",hurt,prot)
+  tiger.addMethod(attackImpl)
+  rep.addClass(tiger)
+  
+  lion = Cl("Lion")
+  lion.setExtend(wild)
+  mane = Type("Mane")
+  maneA = Attr("mane",mane,prot)
+  lion.addAttribute(maneA)
+  lion.addMethod(attackImpl)
+  rep.addClass(lion)
+
+  
+
+  
+  
 
 def createPng(rep):
-  defs = "[User|+Forename;+Surname;+HashedPassword;-Salt|+Login();+Logout()]"
+  #rep = Representation()
+  defs =""# "[User|+Forename;+Surname;+HashedPassword;-Salt|+Login();+Logout()]"
+  for iface in rep.interfaces:
+    iface.strRep = "<<Interface>>;"+iface.name
+    attrs=";"
+    for attr in iface.attributes:
+      if attr.access.access is public:
+        attrs+="+"
+      elif attr.access.access is private:
+        attrs+="-"
+      elif attr.access.access is protected:
+        attrs+="#"
+      attrs+=attr.name + " : " + attr.typeOfAttr.name+";"
+    attrs = attrs[1:-1] 
+    if not (attrs is ""):
+      iface.strRep += "|"+attrs
+    
+    methods=";"
+    for method  in iface.methods:
+      meth=""
+      if method.abstract :
+        meth+="~"
+      if method.access.access is public:
+        meth+="+"
+      elif method.access.access is private:
+        meth+="-"
+      elif method.access.access is protected:
+        meth+="#"
+      meth+=method.name + "("
+      
+      parms="."
+      
+      for param in method.params:
+        parms += param.name+":"+param.typeOfParam.name+"." 
+      
+      parms=parms[1:-1]
+      meth+=parms+"):"+method.returnType.name+";"
+      
+      methods+=meth+";"
+      
+    methods=methods[1:-1]
+    if not (methods is ""):
+      iface.strRep += "|"+methods
+    
+  for cl in rep.classes:
+    if cl.abstract :
+      cl.strRep= "<<Abstract>>;"+cl.name
+    else :
+      cl.strRep = cl.name
+    attrs=";"
+    for attr in cl.attributes:
+      if attr.access.access is public:
+        attrs+="+"
+      elif attr.access.access is private:
+        attrs+="-"
+      elif attr.access.access is protected:
+        attrs+="#"
+      
+      attrs+=attr.name + " : " + attr.typeOfAttr.name+";"
+    attrs = attrs[1:-1]
+    if not (attrs is ""):
+      cl.strRep += "|"+attrs
+    methods=";"
+    for method  in cl.methods:
+      meth=""
+      if method.abstract :
+        meth+="~"
+      if method.access.access is public:
+        meth+="+"
+      elif method.access.access is private:
+        meth+="-"
+      elif method.access.access is protected:
+        meth+="#"
+      meth+=method.name + "("
+      
+      parms="."
+      
+      for param in method.params:
+        parms += param.name+":"+param.typeOfParam.name+"." 
+      
+      parms=parms[1:-1]
+      meth+=parms+"):"+method.returnType.name+";"
+      
+      methods+=meth+";"
+      
+    methods=methods[1:-1]
+    if not (methods is ""):
+      cl.strRep += "|"+methods
+    
+    
+  for iface in rep.interfaces:
+    defs = defs + "["+iface.strRep+"], "
+    
+  for iface in rep.interfaces:
+    for father in iface.extends:
+      defs = defs + "["+father.strRep+"]^-.-["+iface.strRep+"], "
+  for cl in rep.classes:
+    for father in cl.implement:
+      defs = defs + "["+father.strRep+"]^-.-["+cl.strRep+"], "
+  
+  for cl in rep.classes:
+    if cl.extends :
+      defs = defs+ "["+cl.extends.strRep+"]^-["+cl.strRep+"], "
+  print(defs)
+  defs=defs[:-2]
+  print(defs)
   os.system("suml --png \""+defs+"\" > pngs/"+pngName+".png")
 
 
